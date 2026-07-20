c2e7ec85ac1891df3bfd01d4e69753b2508335bf
Hwangseoeun <sunny030921@gmail.com>
2026-07-20 11:01:47 +0900

    feat: #4 BaseEntity 생성

---
+++ common/src/main/java/shop/deal/common/audit/BaseEntity.java
@@ -0,0 +1,29 @@
+package shop.deal.common.audit;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public class BaseEntity {
+
+    @Column(name = "inserted_at", nullable = false, updatable = false)
+    private LocalDateTime insertedAt;
+
+    @Column(name = "updated_at", nullable = false, updatable = true)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.insertedAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
