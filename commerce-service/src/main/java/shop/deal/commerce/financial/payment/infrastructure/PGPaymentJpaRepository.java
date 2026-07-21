5b3f3bf86d0a358dfa411bbd57c942b2cc316baf
KAITOKIDDA <twk031206@naver.com>
2026-07-21 10:39:35 +0900

    feat(financial): #8 Financial BC domain/infrastructure 계층 파일 추가

---
+++ commerce-service/src/main/java/shop/deal/commerce/financial/payment/infrastructure/PGPaymentJpaRepository.java
@@ -0,0 +1,4 @@
+package shop.deal.commerce.financial.payment.infrastructure;
+
+public class PGPaymentJpaRepository {
+}
---
+++ commerce-service/src/main/java/shop/deal/commerce/financial/wallet/domain/WalletLogType.java
@@ -0,0 +1,4 @@
+package shop.deal.commerce.financial.wallet.domain;
+
+public class WalletLogType {
+}
