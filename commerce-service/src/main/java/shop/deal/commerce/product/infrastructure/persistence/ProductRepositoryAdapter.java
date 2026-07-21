9263905d71c4111d66411b3231061efc65affd62
Hwangseoeun <sunny030921@gmail.com>
2026-07-21 19:29:42 +0900

    feat(product): #11 Product(애그리거트 루트) 외 하위 엔티티 Repository 제거
    애그리거트 루트인 `Product` 외의 테이블에는 직접 접근하지 못하도록 ProductImage, Story 관련 Repository를 삭제했습니다.

--- commerce-service/src/main/java/shop/deal/commerce/product/infrastructure/persistence/ProductRepositoryAdapter.java
+++ commerce-service/src/main/java/shop/deal/commerce/product/infrastructure/persistence/ProductRepositoryAdapter.java
@@ -3,16 +3,12 @@ package shop.deal.commerce.product.infrastructure.persistence;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import shop.deal.commerce.product.domain.repository.ProductRepository;
-import shop.deal.commerce.product.infrastructure.persistence.jpa.ProductImageJpaRepository;
 import shop.deal.commerce.product.infrastructure.persistence.jpa.ProductJpaRepository;
-import shop.deal.commerce.product.infrastructure.persistence.jpa.StoryJpaRepository;
 
 @Repository
 @RequiredArgsConstructor
 public class ProductRepositoryAdapter implements ProductRepository {
 
     private final ProductJpaRepository productRepository;
-    private final ProductImageJpaRepository productImageRepository;
-    private final StoryJpaRepository storyRepository;
 
 }
--- commerce-service/src/main/java/shop/deal/commerce/product/infrastructure/persistence/jpa/ProductImageJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package shop.deal.commerce.product.infrastructure.persistence.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import shop.deal.commerce.product.domain.model.ProductImage;
-
-public interface ProductImageJpaRepository extends JpaRepository<ProductImage, Long> {
-}
