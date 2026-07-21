05ad061c4fb087c18c32aec85d74302dfcd6fa37
KAITOKIDDA <twk031206@naver.com>
2026-07-21 10:39:35 +0900

    feat(financial): #8 Financial BC domain/infrastructure 계층 파일 추가

---
+++ commerce-service/src/main/java/shop/deal/commerce/financial/settlement/infrastructure/SettlementPolicyJpaRepository.java
@@ -0,0 +1,4 @@
+package shop.deal.commerce.financial.settlement.infrastructure;
+
+public class SettlementPolicyJpaRepository {
+}
---
+++ commerce-service/src/main/java/shop/deal/commerce/financial/wallet/infrastructure/WalletLogJpaRepository.java
@@ -0,0 +1,4 @@
+package shop.deal.commerce.financial.wallet.infrastructure;
+
+public class WalletLogJpaRepository {
+}
