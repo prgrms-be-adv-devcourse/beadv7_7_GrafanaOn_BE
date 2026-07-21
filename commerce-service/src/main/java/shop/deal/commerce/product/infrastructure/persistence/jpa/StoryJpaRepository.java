8431b5bb0bebf5131c35d2c7e901d94b3f4bf039
Hwangseoeun <sunny030921@gmail.com>
2026-07-21 19:29:42 +0900

    feat(product): #11 Product(애그리거트 루트) 외 하위 엔티티 Repository 제거
    애그리거트 루트인 `Product` 외의 테이블에는 직접 접근하지 못하도록 ProductImage, Story 관련 Repository를 삭제했습니다.

--- commerce-service/src/main/java/shop/deal/commerce/product/infrastructure/persistence/jpa/StoryJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package shop.deal.commerce.product.infrastructure.persistence.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import shop.deal.commerce.product.domain.model.Story;
-
-public interface StoryJpaRepository extends JpaRepository<Story, Long> {
-}
