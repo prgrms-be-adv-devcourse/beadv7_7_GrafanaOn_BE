a2d984a9c9fbbc9412dc7747f04bd9d7a71163b7
KAITOKIDDA <twk031206@naver.com>
2026-07-22 14:38:43 +0900

    feat(financial): #8 결제 애그리거트 도메인 모델 골격 작성

--- commerce-service/src/main/java/shop/deal/commerce/financial/payment/domain/model/PGPayment.java
+++ commerce-service/src/main/java/shop/deal/commerce/financial/payment/domain/model/PGPayment.java
@@ -1,4 +1,54 @@
 package shop.deal.commerce.financial.payment.domain.model;
 
-public class PGPayment {
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import shop.deal.commerce.financial.payment.domain.constant.PGPaymentStatus;
+import shop.deal.common.audit.BaseEntity;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "pg_payment")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PGPayment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "payment_id", nullable = false)
+    private Payment payment;
+
+    @Column(name = "amount", precision = 15, scale = 2, nullable = false)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "state", nullable = false, length = 30)
+    private PGPaymentStatus state;
+
+    @Column(name = "transaction_key", nullable = false, length = 255)
+    private String transaction_key;
+
+    private PGPayment(
+            final Payment payment,
+            final BigDecimal amount,
+            final String transaction_key
+    ) {
+            this.payment = payment;
+            this.amount = amount;
+            this.transaction_key = transaction_key;
+            this.state = PGPaymentStatus.READY;
+    }
+
+    public static PGPayment create(
+            final Payment payment,
+            final BigDecimal amount,
+            final String transaction_key
+    ) {
+            return new PGPayment(payment, amount, transaction_key);
+    }
 }
--- commerce-service/src/main/java/shop/deal/commerce/financial/payment/domain/model/Payment.java
+++ commerce-service/src/main/java/shop/deal/commerce/financial/payment/domain/model/Payment.java
@@ -1,4 +1,63 @@
 package shop.deal.commerce.financial.payment.domain.model;
 
-public class Payment {
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import shop.deal.commerce.financial.payment.domain.constant.PaymentStatus;
+import shop.deal.common.audit.BaseEntity;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "payment")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Payment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(mappedBy = "payment", cascade = CascadeType.PERSIST)
+    private PGPayment pgPayment;
+
+    @Column(name = "wallet_id", nullable = false)
+    private Long walletId;
+
+    // 구매 상황 아니더라도 충전 가능
+    @Column(name = "purchase_id")
+    private Long purchaseId;
+
+    @Column(name = "offer_id")
+    private Long offerId;
+
+    @Column(name = "amount", precision = 15, scale = 2, nullable = false)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "state", nullable = false, length = 30)
+    private PaymentStatus state;
+
+    private Payment(
+            final Long walletId,
+            final Long purchaseId,
+            final Long offerId,
+            final BigDecimal amount
+    ) {
+            this.walletId = walletId;
+            this.purchaseId = purchaseId;
+            this.offerId = offerId;
+            this.amount = amount;
+            this.state = PaymentStatus.PENDING;
+    }
+
+    public static Payment create(
+            final Long walletId,
+            final Long purchased,
+            final Long offerId,
+            final BigDecimal amount
+    ) {
+        return new Payment(walletId, purchased, offerId, amount);
+    }
 }
