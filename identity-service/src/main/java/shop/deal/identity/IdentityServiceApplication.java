947f0f37bf07a882bc17c45e5c8c37855b6419ba
phdcoco <claireluna470@gmail.com>
2026-07-22 10:29:13 +0900

    refactor: #20 identity-service 모듈 리팩토링

--- identity-service/src/main/java/shop/deal/identity/IdentityServiceApplication.java
+++ identity-service/src/main/java/shop/deal/identity/IdentityServiceApplication.java
@@ -1,15 +1,15 @@
-package shop.deal.member;
+package shop.deal.identity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {
 	"shop.deal.common",
-	"shop.deal.member"
+	"shop.deal.identity"
 })
-public class MemberServiceApplication {
+public class IdentityServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MemberServiceApplication.class, args);
+		SpringApplication.run(IdentityServiceApplication.class, args);
 	}
 }
