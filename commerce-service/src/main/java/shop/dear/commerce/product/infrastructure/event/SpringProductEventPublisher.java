b58846a9b860c554e50e531f85a74b63b1ba9420
Hwangseoeun <sunny030921@gmail.com>
2026-07-23 10:15:52 +0900

    refactor(product): #17 이벤트 클래스 위치 이동

--- commerce-service/src/main/java/shop/dear/commerce/product/infrastructure/event/SpringProductEventPublisher.java
+++ commerce-service/src/main/java/shop/dear/commerce/product/infrastructure/event/SpringProductEventPublisher.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 import shop.dear.commerce.product.application.port.ProductEventPublisher;
-import shop.dear.commerce.product.domain.event.ProductChangedEvent;
+import shop.dear.common.event.product.ProductChangedEvent;
 
 @RequiredArgsConstructor
 @Component
