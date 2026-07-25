080788f866e29cd8d5e29e102699af3e0ac487bd
Hwangseoeun <sunny030921@gmail.com>
2026-07-25 23:31:42 +0900

    feat(product): #44 사용자 검증 로직 변경(getMemberProfile -> existsMember)

--- commerce-service/src/main/java/shop/dear/commerce/product/application/port/MemberPort.java
+++ commerce-service/src/main/java/shop/dear/commerce/product/application/port/MemberPort.java
@@ -1,10 +1,11 @@
 package shop.dear.commerce.product.application.port;
 
+import shop.dear.commerce.product.application.dto.external.ExistsMember;
 import shop.dear.commerce.product.application.dto.external.IsSeller;
 import shop.dear.commerce.product.application.dto.external.MemberProfile;
 
 public interface MemberPort {
-
+    ExistsMember existsMember(final Long memberId);
     MemberProfile getMemberProfile(final Long memberId);
     IsSeller isSeller(final Long memberId);
 }
--- commerce-service/src/main/java/shop/dear/commerce/product/infrastructure/client/MemberHttpClient.java
+++ commerce-service/src/main/java/shop/dear/commerce/product/infrastructure/client/MemberHttpClient.java
@@ -6,9 +6,11 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+import shop.dear.commerce.product.application.dto.external.ExistsMember;
 import shop.dear.commerce.product.application.dto.external.IsSeller;
 import shop.dear.commerce.product.application.dto.external.MemberProfile;
 import shop.dear.commerce.product.application.port.MemberPort;
+import shop.dear.commerce.product.infrastructure.client.dto.ExistsMemberApiData;
 import shop.dear.commerce.product.infrastructure.client.dto.IsSellerApiData;
 import shop.dear.commerce.product.infrastructure.client.dto.IsSellerBody;
 import shop.dear.commerce.product.infrastructure.client.dto.MemberProfileApiData;
@@ -22,6 +24,23 @@ public class MemberHttpClient implements MemberPort {
     private final RestClient productRestClient;
 
     @Override
+    public ExistsMember existsMember(final Long memberId) {
+        log.info("[MemberHttpClient] member - 사용자 검증 요청. memberId={}", memberId);
+
+        final ApiResponse<ExistsMemberApiData> body = productRestClient.get()
+            .uri("/internal/members")
+            .retrieve()
+            .body(new ParameterizedTypeReference<>() {
+            });
+
+        final ExistsMemberApiData data = body.getData();
+
+        log.info("[MemberHttpClient] member - 사용자 검증 요청 성공. memberId={}", memberId);
+
+        return new ExistsMember(data.exists());
+    }
+
+    @Override
     public MemberProfile getMemberProfile(final Long memberId) {
         log.info("[MemberHttpClient] member - 프로필 조회 요청. memberId={}", memberId);
 
@@ -33,7 +52,7 @@ public class MemberHttpClient implements MemberPort {
 
         final MemberProfileApiData data = body.getData();
 
-        log.info("[MemberHttpClient] member 프로필 조회 요청 성공. memberId={}", memberId);
+        log.info("[MemberHttpClient] member - 프로필 조회 요청 성공. memberId={}", memberId);
 
         return new MemberProfile(
             data.name(),
