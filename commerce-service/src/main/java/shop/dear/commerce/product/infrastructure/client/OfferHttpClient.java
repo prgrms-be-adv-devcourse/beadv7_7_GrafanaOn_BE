1f3c926d24b35404e7f7fdb8e2c7b80a33f9480c
Hwangseoeun <sunny030921@gmail.com>
2026-07-25 22:37:40 +0900

    feat(product): #44 이벤트 발행 및 rest api 통신 구간에 로깅 추가

--- commerce-service/src/main/java/shop/dear/commerce/product/infrastructure/client/OfferHttpClient.java
+++ commerce-service/src/main/java/shop/dear/commerce/product/infrastructure/client/OfferHttpClient.java
@@ -1,6 +1,7 @@
 package shop.dear.commerce.product.infrastructure.client;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
@@ -9,7 +10,7 @@ import shop.dear.commerce.product.application.port.OfferPort;
 import shop.dear.commerce.product.infrastructure.client.dto.ExistsOfferApiData;
 import shop.dear.common.response.ApiResponse;
 
-
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class OfferHttpClient implements OfferPort {
@@ -18,6 +19,8 @@ public class OfferHttpClient implements OfferPort {
 
     @Override
     public ExistsOffer existsOffer(final Long productId) {
+        log.info("[OfferHttpClient] offer - 오퍼 존재 여부 조회 요청. productId={}", productId);
+
         final ApiResponse<ExistsOfferApiData> body = productRestClient.get()
             .uri("/offers/{productId}/status")
             .retrieve()
@@ -25,6 +28,8 @@ public class OfferHttpClient implements OfferPort {
             });
 
         final ExistsOfferApiData data = body.getData();
+
+        log.info("[OfferHttpClient] offer - 오퍼 존재 여부 조회 요청 성공. productId={} exists={}", productId, data.exists());
 
         return new ExistsOffer(data.exists());
     }
