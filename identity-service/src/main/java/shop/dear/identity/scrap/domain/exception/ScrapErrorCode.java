62187065d12b7df5eebf416890b9f8a6e823dcf9
SehaKim <aiostpch@naver.com>
2026-07-25 22:50:56 +0900

    feat(member): #34 스크랩 추가 시 중복검증

--- identity-service/src/main/java/shop/dear/identity/scrap/domain/exception/ScrapErrorCode.java
+++ identity-service/src/main/java/shop/dear/identity/scrap/domain/exception/ScrapErrorCode.java
@@ -9,6 +9,7 @@ import shop.dear.common.response.ErrorCode;
 @Getter
 public enum ScrapErrorCode implements ErrorCode {
     SCRAP_NOT_FOUND("SC-001", "존재하지 않는 스크랩입니다."),
+    DUPLICATE_SCRAP("SC-002", "이미 스크랩한 상품입니다."),
     ;
 
     private final String value;
