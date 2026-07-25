e355f7071050e28c182c092035ad5bd075fb244a
SehaKim <aiostpch@naver.com>
2026-07-25 16:07:15 +0900

    feat(member): #34 페이징처리

--- identity-service/src/main/java/shop/dear/identity/scrap/domain/repository/ScrapRepository.java
+++ identity-service/src/main/java/shop/dear/identity/scrap/domain/repository/ScrapRepository.java
@@ -1,13 +1,14 @@
 package shop.dear.identity.scrap.domain.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import shop.dear.identity.scrap.domain.model.Scrap;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ScrapRepository {
     Scrap save(Scrap scrap);
     void deleteById(Long id);
     Optional<Scrap> findByMemberIdAndProductId(Long memberId, Long productId);
-    List<Scrap> findByMemberIdOrderByInsertedAt(Long memberId);
+    Page<Scrap> findByMemberId(Long memberId, Pageable pageable);
 }
--- identity-service/src/main/java/shop/dear/identity/scrap/presentation/dto/ScrapResponse.java
+++ identity-service/src/main/java/shop/dear/identity/scrap/presentation/dto/ScrapResponse.java
@@ -2,9 +2,9 @@ package shop.dear.identity.scrap.presentation.dto;
 
 import shop.dear.identity.scrap.application.dto.ScrapInfo;
 
-public record ScrapResponse(Long id, Long productId) {
+public record ScrapResponse(Long productId) {
 
     public static ScrapResponse from(final ScrapInfo info) {
-        return new ScrapResponse(info.id(), info.productId());
+        return new ScrapResponse(info.productId());
     }
 }
