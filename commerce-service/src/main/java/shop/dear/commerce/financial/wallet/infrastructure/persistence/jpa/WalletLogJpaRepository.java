f0a8d5feef2565b4a912cd0f13179208a9a7cc07
KAITOKIDDA <twk031206@naver.com>
2026-07-27 02:36:15 +0900

    feat(financial): #31 예치금 애그리거트 영속성 연결

--- commerce-service/src/main/java/shop/dear/commerce/financial/wallet/infrastructure/persistence/jpa/WalletLogJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package shop.dear.commerce.financial.wallet.infrastructure.persistence.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import shop.dear.commerce.financial.wallet.domain.model.WalletLog;
-
-public interface WalletLogJpaRepository extends JpaRepository<WalletLog, Long> {
-}
