137cb1c18c5fc1d0d4ed482e9a2990e2ab9f7566
SehaKim <aiostpch@naver.com>
2026-07-27 15:20:10 +0900

    feat(member): #59 유효한 사용자 검증 API 구현

--- identity-service/src/main/java/shop/dear/identity/member/presentation/InternalMemberController.java
+++ identity-service/src/main/java/shop/dear/identity/member/presentation/InternalMemberController.java
@@ -9,6 +9,7 @@ import shop.dear.identity.member.application.MemberService;
 import shop.dear.identity.member.presentation.dto.request.CreateProfileRequest;
 import shop.dear.identity.member.presentation.dto.response.CreateProfileResponse;
 import shop.dear.identity.member.presentation.dto.response.SellerCheckResponse;
+import shop.dear.identity.member.presentation.dto.response.MemberCheckResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +19,7 @@ public class InternalMemberController {
     private final MemberService memberService;
 
     @PostMapping
-    public ApiResponse<CreateProfileResponse> createProfile(@Valid @RequestBody final CreateProfileRequest request){
+    public ApiResponse<CreateProfileResponse> createProfile(@Valid @RequestBody final CreateProfileRequest request) {
 
         CreateProfileResponse response = CreateProfileResponse.from(memberService.createProfile(request.toCommand()));
 
@@ -32,4 +33,12 @@ public class InternalMemberController {
 
         return ApiResponse.successWithData(SellerCheckResponse.from(isSeller));
     }
+    @GetMapping
+    public ApiResponse<MemberCheckResponse> existsMember(@AuthUser final Long memberId) {
+
+        boolean exists = memberService.existsMember(memberId);
+
+        return ApiResponse.successWithData(MemberCheckResponse.from(exists));
+    }
+
 }
