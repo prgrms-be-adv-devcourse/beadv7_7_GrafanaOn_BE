9f3345c87bba195a4977c9df421234273fcdecaf
KAITOKIDDA <twk031206@naver.com>
2026-07-28 22:58:54 +0900

    feat(financial): #53 결제 요청 및 상태 조회 API 추가

--- commerce-service/src/main/java/shop/dear/commerce/financial/payment/presentation/dto/request/PaymentRequest.java
+++ commerce-service/src/main/java/shop/dear/commerce/financial/payment/presentation/dto/request/PaymentRequest.java
@@ -1,4 +1,30 @@
 package shop.dear.commerce.financial.payment.presentation.dto.request;
 
-public class PaymentRequest {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import shop.dear.commerce.financial.payment.application.dto.PayOrderCommand;
+import shop.dear.common.event.order.OrderType;
+
+import java.math.BigDecimal;
+
+public record PaymentRequest(
+        @NotNull
+        @Positive
+        Long orderId,
+
+        @NotNull
+        OrderType orderType,
+
+        @NotNull
+        @Positive
+        BigDecimal amount
+) {
+    public PayOrderCommand toCommand(final Long memberId) {
+        return new PayOrderCommand(
+                memberId,
+                orderId,
+                orderType,
+                amount
+        );
+    }
 }
