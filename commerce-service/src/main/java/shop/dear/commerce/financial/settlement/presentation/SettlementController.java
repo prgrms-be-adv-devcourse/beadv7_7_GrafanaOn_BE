315ff87e308dcc18a949c73859233282568eeea0
SehaKim <81903033+aio19581@users.noreply.github.com>
2026-07-28 16:07:44 +0900

    feat(financial): #75 정산 이력 조회

--- commerce-service/src/main/java/shop/dear/commerce/financial/settlement/presentation/SettlementController.java
+++ commerce-service/src/main/java/shop/dear/commerce/financial/settlement/presentation/SettlementController.java
@@ -7,9 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import shop.dear.commerce.financial.settlement.application.SettlementService;
 import shop.dear.commerce.financial.settlement.presentation.dto.response.NetAmountResponse;
+import shop.dear.commerce.financial.settlement.presentation.dto.response.SettlementResponse;
 import shop.dear.common.response.ApiResponse;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.YearMonth;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static shop.dear.common.response.ApiResponse.successWithData;
 
@@ -22,10 +27,24 @@ public class SettlementController {
 
 	//정산예정금액 조회
 	@GetMapping("/me")
-	public ResponseEntity<ApiResponse<NetAmountResponse>> getNetAmount(Long walletId, YearMonth targetMonth) {
+	public ResponseEntity<ApiResponse<NetAmountResponse>> getNetAmount(Long memberId, YearMonth targetMonth) {
 
-		NetAmountResponse netAmountResponse = NetAmountResponse.from(settlementService.getSettlementAmount(walletId, targetMonth));
+		NetAmountResponse netAmountResponse = NetAmountResponse.from(settlementService.getSettlementAmount(memberId, targetMonth));
 
 		return ResponseEntity.ok(successWithData(netAmountResponse));
+	}
+
+	@GetMapping("/me/history")
+	public ResponseEntity<ApiResponse<List<SettlementResponse>>> getHistory(Long memberId, LocalDateTime startDate, LocalDateTime endDate) {
+
+		List<SettlementResponse> settlementResponse = settlementService.getSettlements(
+			memberId,
+			startDate,
+			endDate
+		).stream()
+		.map(SettlementResponse::from)
+		.toList();
+		
+		return ResponseEntity.ok(successWithData(settlementResponse));
 	}
 }
