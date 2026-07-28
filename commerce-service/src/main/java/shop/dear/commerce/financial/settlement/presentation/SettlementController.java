941fc8946dc01b7e9a1ea4932d639922cf31736b
SehaKim <81903033+aio19581@users.noreply.github.com>
2026-07-28 16:44:55 +0900

    refactor(financial): #75 api endpoint 및 코드 스타일 수정

--- commerce-service/src/main/java/shop/dear/commerce/financial/settlement/presentation/SettlementController.java
+++ commerce-service/src/main/java/shop/dear/commerce/financial/settlement/presentation/SettlementController.java
@@ -1,5 +1,6 @@
 package shop.dear.commerce.financial.settlement.presentation;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,43 +9,48 @@ import org.springframework.web.bind.annotation.RestController;
 import shop.dear.commerce.financial.settlement.application.SettlementService;
 import shop.dear.commerce.financial.settlement.presentation.dto.response.NetAmountResponse;
 import shop.dear.commerce.financial.settlement.presentation.dto.response.SettlementResponse;
+import shop.dear.common.auth.AuthUser;
 import shop.dear.common.response.ApiResponse;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.YearMonth;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static shop.dear.common.response.ApiResponse.successWithData;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/settlement")
+@RequestMapping("/api/settlements")
 public class SettlementController {
 
 	private final SettlementService settlementService;
 
-	//정산예정금액 조회
+	//특정월의 정산예정금액 조회 (전월 1일 ~ 말일)
 	@GetMapping("/me")
-	public ResponseEntity<ApiResponse<NetAmountResponse>> getNetAmount(Long memberId, YearMonth targetMonth) {
+	public ResponseEntity<ApiResponse<NetAmountResponse>> getNetAmount(@AuthUser Long memberId, @NotNull YearMonth targetMonth) {
 
-		NetAmountResponse netAmountResponse = NetAmountResponse.from(settlementService.getSettlementAmount(memberId, targetMonth));
+		NetAmountResponse netAmountResponse = NetAmountResponse.from(settlementService.getNetAmount(memberId, targetMonth));
 
 		return ResponseEntity.ok(successWithData(netAmountResponse));
 	}
 
+	//특정기간 동안의 정산완료 이력 조회
 	@GetMapping("/me/history")
-	public ResponseEntity<ApiResponse<List<SettlementResponse>>> getHistory(Long memberId, LocalDateTime startDate, LocalDateTime endDate) {
+	public ResponseEntity<ApiResponse<List<SettlementResponse>>> getHistory(
+		@AuthUser Long memberId,
+		@NotNull LocalDateTime startDate,
+		@NotNull LocalDateTime endDate
+	) {
 
-		List<SettlementResponse> settlementResponse = settlementService.getSettlements(
-			memberId,
-			startDate,
-			endDate
-		).stream()
-		.map(SettlementResponse::from)
-		.toList();
+		List<SettlementResponse> history = settlementService.getHistory(
+				memberId,
+				startDate,
+				endDate
+			)
+			.stream()
+			.map(SettlementResponse::from)
+			.toList();
 		
-		return ResponseEntity.ok(successWithData(settlementResponse));
+		return ResponseEntity.ok(successWithData(history));
 	}
 }
