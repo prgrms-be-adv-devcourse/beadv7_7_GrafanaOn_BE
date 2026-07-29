87cb185d490b604c6fe3cae9c59d9219f28d3bd9
SehaKim <aiostpch@naver.com>
2026-07-30 08:52:17 +0900

    refactor(financial): #75 config 수정

--- commerce-service/src/main/java/shop/dear/commerce/financial/settlement/infrastructure/client/SettlementClientConfig.java
+++ commerce-service/src/main/java/shop/dear/commerce/financial/settlement/infrastructure/client/SettlementClientConfig.java
@@ -1,17 +1,20 @@
 package shop.dear.commerce.financial.settlement.infrastructure.client;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
+import shop.dear.common.client.InternalRestClientFactory;
 
 @Configuration
+@RequiredArgsConstructor
 public class SettlementClientConfig {
+	private final InternalRestClientFactory internalRestClientFactory;
 
 	@Bean
 	RestClient walletRestClient(@Value("${wallet.client.base-url}") String baseUrl) {
-		return RestClient.builder()
-			.baseUrl(baseUrl)
-			.build();
+
+		return internalRestClientFactory.builder(baseUrl).build();
 	}
 }
