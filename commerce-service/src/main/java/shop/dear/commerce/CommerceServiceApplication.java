5e7de351aef5e405ff204040bec9e74f30b57546
Hwangseoeun <sunny030921@gmail.com>
2026-07-29 15:34:44 +0900

    refactor(product): #101 spring scheduling 외부 인프라 분리를 위해 설정 위치 변경

--- commerce-service/src/main/java/shop/dear/commerce/CommerceServiceApplication.java
+++ commerce-service/src/main/java/shop/dear/commerce/CommerceServiceApplication.java
@@ -2,9 +2,7 @@ package shop.dear.commerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication(scanBasePackages = {
 	"shop.dear.common",
 	"shop.dear.commerce"
