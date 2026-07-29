fe5c51f1ae0722ebc616642855a06c9e47b6ac44
phdcoco <claireluna470@gmail.com>
2026-07-29 16:40:46 +0900

    refactor: #108 Identity의 MemberClientConfig도 팩토리를 사용하도록 리팩토링

--- identity-service/src/main/java/shop/dear/identity/member/infrastructure/client/MemberClientConfig.java
+++ identity-service/src/main/java/shop/dear/identity/member/infrastructure/client/MemberClientConfig.java
@@ -1,18 +1,20 @@
 package shop.dear.identity.member.infrastructure.client;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
+import shop.dear.common.client.InternalRestClientFactory;
 
 @Configuration
+@RequiredArgsConstructor
 public class MemberClientConfig {
+    private final InternalRestClientFactory internalRestClientFactory;
 
     @Bean
     RestClient productRestClient(@Value("${identity.client.commerce-base-url}") String baseUrl) {
 
-        return RestClient.builder()
-            .baseUrl(baseUrl)
-            .build();
+        return internalRestClientFactory.builder(baseUrl).build();
     }
 }
