ac48525c1164e5bbc988c34d52d1fb41948ec444
Hwangseoeun <sunny030921@gmail.com>
2026-07-29 15:24:22 +0900

    refactor(product): #101 spring scheduling 사용을 위한 @EnableScheduling 어노테이션 추가

--- commerce-service/src/main/java/shop/dear/commerce/CommerceServiceApplication.java
+++ commerce-service/src/main/java/shop/dear/commerce/CommerceServiceApplication.java
@@ -2,7 +2,9 @@ package shop.dear.commerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = {
 	"shop.dear.common",
 	"shop.dear.commerce"
