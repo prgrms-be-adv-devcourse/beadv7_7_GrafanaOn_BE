cc824fc8c75b5ebdd87ef89840f264c93d60b3b8
Hwangseoeun <sunny030921@gmail.com>
2026-07-29 22:52:43 +0900

    refactor(common): #101 ApiResponse 내 기본 생성자 생성 어노테이션 추가

--- common/src/main/java/shop/dear/common/response/ApiResponse.java
+++ common/src/main/java/shop/dear/common/response/ApiResponse.java
@@ -1,20 +1,23 @@
 package shop.dear.common.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ApiResponse<T> {
 
     private static final String SUCCESS_CODE = "success";
     private static final String SUCCESS_MESSAGE = "API 요청이 정상적으로 수행되었습니다.";
 
-    private final String code;
+    private String code;
 
-    private final String message;
+    private String message;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    private final T data;
+    private T data;
 
     public static ApiResponse<Void> success() {
         return new ApiResponse<>(SUCCESS_CODE, SUCCESS_MESSAGE, null);
