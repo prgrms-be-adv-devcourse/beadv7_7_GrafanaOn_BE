fee4ffd66000d331c9ae1ac00c53b6f2bbabbf2a
phdcoco <claireluna470@gmail.com>
2026-07-29 10:07:38 +0900

    feat: #81 JWT 검증을 위한 정보 수집 코드 구현

--- gateway-service/src/main/java/shop/dear/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/shop/dear/gateway/GatewayApplication.java
@@ -2,7 +2,10 @@ package shop.dear.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+// 이게 shop.dear.gateway 하위의 GatewayJwtProperties를 찾아 Bean으로 등록
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class GatewayApplication {
 
