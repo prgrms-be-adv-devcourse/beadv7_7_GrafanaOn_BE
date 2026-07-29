9e1628d91aa01708dab1605037a1173456a9bcab
phdcoco <claireluna470@gmail.com>
2026-07-29 16:23:12 +0900

    feat: #108 common 모듈에 릴레이 규칙 인터셉터 추가

---
+++ common/src/main/java/shop/dear/common/client/InternalCallInterceptor.java
@@ -0,0 +1,30 @@
+package shop.dear.common.client;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import shop.dear.common.auth.AuthUser;
+
+import java.io.IOException;
+
+@Component
+public class InternalCallInterceptor implements ClientHttpRequestInterceptor {
+    @Override
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+        if (RequestContextHolder.getRequestAttributes() instanceof ServletRequestAttributes attributes) {
+            String memberId = attributes.getRequest()
+                    .getHeader(AuthUser.MEMBER_ID_HEADER);
+
+            if (StringUtils.hasText(memberId)) {
+                request.getHeaders().set(AuthUser.MEMBER_ID_HEADER, memberId);
+            }
+        }
+
+        return execution.execute(request, body);
+    }
+}
