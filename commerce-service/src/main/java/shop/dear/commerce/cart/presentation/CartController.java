7d6c4d6d0fcd07e9160cd1e98fdb76155630fd65
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2026-07-29 21:00:51 +0900

    feat(cart): #91 장바구니 상품 조회 API 추가

--- commerce-service/src/main/java/shop/dear/commerce/cart/presentation/CartController.java
+++ commerce-service/src/main/java/shop/dear/commerce/cart/presentation/CartController.java
@@ -1,14 +1,32 @@
 package shop.dear.commerce.cart.presentation;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import shop.dear.commerce.cart.application.CartService;
+import shop.dear.commerce.cart.application.dto.GetAllCartItemProductResponse;
+import shop.dear.common.auth.AuthUser;
+import shop.dear.common.response.ApiResponse;
+
+import static shop.dear.common.response.ApiResponse.successWithData;
 
 @Validated
 @RequiredArgsConstructor
 @RequestMapping("/api/carts")
 @RestController
 public class CartController {
+
+    private final CartService cartService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<GetAllCartItemProductResponse>> findAllCartItems(
+            @AuthUser final Long memberId
+    ) {
+        final GetAllCartItemProductResponse cartItems = cartService.getCartItems(memberId);
+        return ResponseEntity.ok(successWithData(cartItems));
+    }
 
 }
