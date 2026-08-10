d4f1983a3855cb62ac93992365f75582eac9a3b2
bum0w0 <kgy5988@naver.com>
2026-08-10 16:53:40 +0900

    feat(order): #154 Purchase 이벤트 발행 구조 정리

--- commerce-service/src/main/java/shop/dear/commerce/order/purchase/application/port/PurchaseEventPublisher.java
+++ commerce-service/src/main/java/shop/dear/commerce/order/purchase/application/port/PurchaseEventPublisher.java
@@ -2,11 +2,14 @@ package shop.dear.commerce.order.purchase.application.port;
 
 import shop.dear.common.event.financial.PaymentRequestedEvent;
 import shop.dear.common.event.order.FinishedOrderEvent;
+import shop.dear.common.event.order.PurchaseReleasedEvent;
 
 public interface PurchaseEventPublisher {
 
   void publish(FinishedOrderEvent event);
 
   void publish(PaymentRequestedEvent event);
+
+  void publish(PurchaseReleasedEvent event);
 
 }
