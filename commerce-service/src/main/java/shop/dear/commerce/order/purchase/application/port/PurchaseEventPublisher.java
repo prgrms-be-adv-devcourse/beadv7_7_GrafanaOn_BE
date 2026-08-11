410899435e82292d44afcbdad06de2f16c14dafb
bum0w0 <kgy5988@naver.com>
2026-08-11 17:12:37 +0900

    refactor(order): #154 사용하지 않는 이벤트 발행 코드 제거

--- commerce-service/src/main/java/shop/dear/commerce/order/purchase/application/port/PurchaseEventPublisher.java
+++ commerce-service/src/main/java/shop/dear/commerce/order/purchase/application/port/PurchaseEventPublisher.java
@@ -2,14 +2,11 @@ package shop.dear.commerce.order.purchase.application.port;
 
 import shop.dear.common.event.financial.PaymentRequestedEvent;
 import shop.dear.common.event.order.FinishedOrderEvent;
-import shop.dear.common.event.order.PurchaseReleasedEvent;
 
 public interface PurchaseEventPublisher {
 
   void publish(FinishedOrderEvent event);
 
   void publish(PaymentRequestedEvent event);
-
-  void publish(PurchaseReleasedEvent event);
 
 }
--- commerce-service/src/main/java/shop/dear/commerce/order/purchase/infrastructure/event/SpringPurchaseEventPublisher.java
+++ commerce-service/src/main/java/shop/dear/commerce/order/purchase/infrastructure/event/SpringPurchaseEventPublisher.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 import shop.dear.commerce.order.purchase.application.port.PurchaseEventPublisher;
 import shop.dear.common.event.financial.PaymentRequestedEvent;
 import shop.dear.common.event.order.FinishedOrderEvent;
-import shop.dear.common.event.order.PurchaseReleasedEvent;
 
 @Component
 @RequiredArgsConstructor
@@ -21,11 +20,6 @@ public class SpringPurchaseEventPublisher implements PurchaseEventPublisher {
 
   @Override
   public void publish(PaymentRequestedEvent event) {
-    eventPublisher.publishEvent(event);
-  }
-
-  @Override
-  public void publish(PurchaseReleasedEvent event) {
     eventPublisher.publishEvent(event);
   }
 }
