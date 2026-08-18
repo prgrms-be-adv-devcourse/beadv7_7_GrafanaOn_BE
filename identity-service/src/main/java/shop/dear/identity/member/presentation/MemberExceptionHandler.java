ae7af21637ede6425babf8f4d1fd871329256257
SehaKim <81903033+aio19581@users.noreply.github.com>
2026-08-18 12:06:49 +0900

    fix(member): #191 NPE 가드 추가

--- identity-service/src/main/java/shop/dear/identity/member/presentation/MemberExceptionHandler.java
+++ identity-service/src/main/java/shop/dear/identity/member/presentation/MemberExceptionHandler.java
@@ -49,7 +49,7 @@ public class MemberExceptionHandler{
 
         String message = e.getMostSpecificCause().getMessage();
 
-        if (message.contains("uk_member_nickname")) {
+        if (message != null && message.contains("uk_member_nickname")) {
             log.warn("닉네임 중복 발생", e);
             return ResponseEntity.status(HttpStatus.CONFLICT)
                 .body(fail(MemberErrorCode.DUPLICATE_NICKNAME));
