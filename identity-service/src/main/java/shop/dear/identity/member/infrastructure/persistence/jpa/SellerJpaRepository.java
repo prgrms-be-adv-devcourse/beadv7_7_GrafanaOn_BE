fcd88f0b3eba2c44c4bb5ec77c692c90aea0b000
SehaKim <aiostpch@naver.com>
2026-08-19 00:28:44 +0900

    feature(member): #163 스케줄러 구현

--- identity-service/src/main/java/shop/dear/identity/member/infrastructure/persistence/jpa/SellerJpaRepository.java
+++ identity-service/src/main/java/shop/dear/identity/member/infrastructure/persistence/jpa/SellerJpaRepository.java
@@ -1,11 +1,16 @@
 package shop.dear.identity.member.infrastructure.persistence.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import shop.dear.identity.member.domain.constract.SellerStatus;
 import shop.dear.identity.member.domain.model.Seller;
 
-import java.util.Optional;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface SellerJpaRepository extends JpaRepository<Seller, Long> {
 
-    Optional<Seller> findByMemberId(final long memberId);
+	List<Seller> findByStatusAndWithdrawnAtBefore(
+		SellerStatus status,
+		LocalDateTime withdrawnAtBefore
+	);
 }
