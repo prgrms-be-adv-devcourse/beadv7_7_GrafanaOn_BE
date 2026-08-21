ce21617a132cfda9adf00e50c2171659f4635f11
SehaKim <81903033+aio19581@users.noreply.github.com>
2026-08-21 12:26:00 +0900

    feat(settlement): #205 스케줄러 분산락 적용

--- commerce-service/src/main/java/shop/dear/commerce/financial/settlement/infrastructure/scheduler/SettlementScheduler.java
+++ commerce-service/src/main/java/shop/dear/commerce/financial/settlement/infrastructure/scheduler/SettlementScheduler.java
@@ -2,6 +2,7 @@ package shop.dear.commerce.financial.settlement.infrastructure.scheduler;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import shop.dear.commerce.financial.settlement.application.SettlementService;
@@ -18,6 +19,7 @@ public class SettlementScheduler {
 
 	//매월 1일 오전 5시 실행
 	@Scheduled(cron = "0 0 5 1 * * ")
+	@SchedulerLock(name = "settlement_payout", lockAtMostFor = "30m", lockAtLeastFor = "1m")
 	public void payoutTaskByCron() {
 
 		YearMonth targetMonth = YearMonth.now();
