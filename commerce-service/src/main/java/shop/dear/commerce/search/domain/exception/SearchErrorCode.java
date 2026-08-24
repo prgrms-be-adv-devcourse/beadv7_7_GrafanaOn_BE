9f2feecd0649dd64133756233f10b4b69cd97d58
phdcoco <claireluna470@gmail.com>
2026-08-24 11:29:42 +0900

    feat: #220 예외 코드 추가

--- commerce-service/src/main/java/shop/dear/commerce/search/domain/exception/SearchErrorCode.java
+++ commerce-service/src/main/java/shop/dear/commerce/search/domain/exception/SearchErrorCode.java
@@ -9,6 +9,7 @@ import shop.dear.common.response.ErrorCode;
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 public enum SearchErrorCode implements ErrorCode {
     REINDEX_ALREADY_RUNNING("SR-001", "재색인이 이미 실행 중입니다."),
+    SEARCH_TEMPORARILY_UNAVAILABLE("SR-002", "검색이 일시적으로 제한되고 있습니다. 잠시 후 다시 시도해주세요."),
     ;
 
     private final String value;
