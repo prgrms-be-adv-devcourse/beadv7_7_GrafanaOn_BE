54554efeed5d4db1a7bff46a7db1720d0dbe65c3
SehaKim <81903033+aio19581@users.noreply.github.com>
2026-08-28 15:47:14 +0900

    refactor(recommendation): #218 임베딩조정

--- recommendation-service/src/main/java/shop/dear/recommendation/infrastructure/ai/SpringAiProductEmbedder.java
+++ recommendation-service/src/main/java/shop/dear/recommendation/infrastructure/ai/SpringAiProductEmbedder.java
@@ -1,12 +1,17 @@
 package shop.dear.recommendation.infrastructure.ai;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
+import shop.dear.common.exception.BusinessException;
 import shop.dear.recommendation.domain.model.Embedding;
 import shop.dear.recommendation.domain.repository.ProductEmbedder;
+
+import java.util.List;
 
 @Slf4j
 @Component
@@ -16,20 +21,34 @@ public class SpringAiProductEmbedder implements ProductEmbedder {
 	// EmbeddingModel : 텍스트를 유사도 계산과 벡터로 변환해주는 추상화 인터페이스
 	private final EmbeddingModel embeddingModel;
 	private final String modelName;
+	//유사도가 잘 안나와서 추가 중...
+	private final DocumentNormalizer documentNormalizer;
 
 	// model_name은 로컬과 운영이 다르므로 환경변수로 주입
 	public SpringAiProductEmbedder(
 		final EmbeddingModel embeddingModel,
-		@Value("${recommendation.embedding.model-name}") final String modelName
+		@Value("${recommendation.embedding.model-name}") final String modelName,
+		final DocumentNormalizer documentNormalizer
 	)
 	{
 		this.embeddingModel = embeddingModel;
 		this.modelName = modelName;
+		this.documentNormalizer = documentNormalizer;
 	}
 
 	@Override
 	public Embedding embed(final String story) {
 
-		return new Embedding(this.modelName, this.embeddingModel.embed(story));
+		//STORY 정규화 필요
+		final String normalized = documentNormalizer
+			.apply(List.of(new Document(story)))
+			.getFirst()
+			.getText();
+
+		if (normalized != null) {
+			return new Embedding(this.modelName, this.embeddingModel.embed(normalized));
+		} else {
+			return  null;
+		}
 	}
 }
