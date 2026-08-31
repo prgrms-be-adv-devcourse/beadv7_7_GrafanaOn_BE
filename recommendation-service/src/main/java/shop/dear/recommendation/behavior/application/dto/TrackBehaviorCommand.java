ebdbc96762084a827e905269f14cb5e9da3a22fc
Hwa Young Yang <110398814+young0311@users.noreply.github.com>
2026-08-31 23:05:09 +0900

    fix: 행동 이벤트 Command 생성 시 인증 사용자 전달 구조 정합성 수정

--- recommendation-service/src/main/java/shop/dear/recommendation/behavior/application/dto/TrackBehaviorCommand.java
+++ recommendation-service/src/main/java/shop/dear/recommendation/behavior/application/dto/TrackBehaviorCommand.java
@@ -1,8 +1,6 @@
 package shop.dear.recommendation.behavior.application.dto;
 
 import shop.dear.recommendation.behavior.domain.constant.BehaviorType;
-import shop.dear.recommendation.behavior.domain.model.RecommendationBehaviorEvent;
-import shop.dear.recommendation.behavior.presentation.dto.TrackBehaviorRequest;
 
 import java.time.LocalDateTime;
 
